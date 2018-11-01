c0c7d505d28fba987620942bff71d9073a8ecbc2
Naruya Kondo <n-kondo@g.ecc.u-tokyo.ac.jp>
2018-11-02 03:57:34 +0900

    ships.task変数を追加。船何台が点検/修理に回っているかをカウントするため

--- ships.py
+++ ships.py
@@ -3,5 +3,8 @@ class Ships():
 		# この船がどこの風車にいるか
 		# 港に残り続けるなら -1
 		self.target_windfarm = target_windfarm
-		# 港にいるかどうか
+		# 港にいるかどうか。target_windfarmがあっても夜寄港しているときに大事になる変数。
 		self.stay_harbor = False
+		# 何してるか。None or 'inspection' or 'repair'
+		# 全体で何隻の船が点検にあたっているかをカウントする際に使う。
+		self.task = None
