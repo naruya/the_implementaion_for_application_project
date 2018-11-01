0d60c0fda2257623a3d3591cd750394ac5e8e896
Naruya Kondo <n-kondo@g.ecc.u-tokyo.ac.jp>
2018-11-02 04:08:05 +0900

    各タイムステップで呼ぶwf.check_present_situationとwf全体を一括管理する関数の簡素化

--- windfarm_state.py
+++ windfarm_state.py
@@ -10,28 +10,44 @@ class Windfarm_state():
 		self.all_windfarm = [Windfarm(k, environment.wind_hist) \
 							for k in range(total_windfarm_num)]
         
+# 	# 総じて使われていないので削除。
+# 	# また、wf.check_present_situationに引数stateは渡せないようにした。
+# 	# これは、time_step関数内でwfの更新に、
+# 	# ship_planから受け取ったわけではなくwindfarm内で用意された変数`state`を
+# 	# 引数とするべきではなく（windfarmは受動的な設計）、
+# 	# またship_planからstateを受け取ることもない
+# 	#（ship_planの行動はwfにstateを渡すことで表現されるのではなく、
+# 	# 船を出航することで表現されている）
+# 	# ため。
+# 	
+# 	def check_need_inspection_all(self):
+# 		return [wf.return_check_present_situation('inspection')[0] \
+# 				for wf in self.all_windfarm]
+
+# 	def check_need_repair_all(self):
+# 		return [wf.return_check_present_situation('repair')[0] \
+# 				for wf in self.all_windfarm]
+
+# 	def check_progress_inspection_all(self):
+# 		return [wf.return_check_present_situation('inspection')[1] \
+# 				for wf in self.all_windfarm]
+
+# 	def check_progress_repair_all(self):
+# 		return [wf.return_check_present_situation('repair')[1] \
+# 				for wf in self.all_windfarm]
+
+	# ship_planで参照される。
 	def check_need_inspection_all(self):
-		return [wf.return_check_present_situation('inspection')[0] \
-				for wf in self.all_windfarm]
-
+		return [wf.need_inspection for wf in self.all_windfarm]
+    
+	# ship_planで参照される。
 	def check_need_repair_all(self):
-		return [wf.return_check_present_situation('repair')[0] \
-				for wf in self.all_windfarm]
+		return [wf.need_repair for wf in self.all_windfarm]
 
-	def check_progress_inspection_all(self):
-		return [wf.return_check_present_situation('inspection')[1] \
-				for wf in self.all_windfarm]
+	def check_time_from_last_inspection_all(self):
+		return [wf.time_from_last_inspection for wf in self.all_windfarm]
 
-	def check_progress_repair_all(self):
-		return [wf.return_check_present_situation('repair')[1] \
-				for wf in self.all_windfarm]
-
-	def time_from_last_inspection_all(self):
-		tmp = [wf.time_from_last_inspection
-				for wf in self.all_windfarm]
-		return tmp
-
-	def check_where_is_ships(self):
+	def check_there_is_ship_all(self):
 		return [wf.there_is_ship for wf in self.all_windfarm]
 
 	# 動ける風車の数を返す
@@ -49,13 +65,5 @@ class Windfarm_state():
 		return sum([wf.generated_power for wf in self.all_windfarm]) * 36
 
 	def time_step(self, t):
-		for wd in self.all_windfarm:
-#             if need_repair:
-#                 state = 'repair'
-#             if 
-			wd.check_present_situation('inspection', t, \
-										self.environment.can_work)
-			"""
-			wd.check_present_situation('repair', t, \
-										self.environment.can_work)
-			"""
+		for wf in self.all_windfarm:
+			wf.check_present_situation(t, self.environment.can_work)
